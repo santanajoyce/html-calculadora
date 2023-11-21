5d07f07b859c98d6061d871be7bfde8f3b9681b0
joyce patricia pinheiro <joycework10@gmail.com>
2023-11-21 09:14:10 -0300

    calculadora javascript

--- script.js
+++ script.js
@@ -6,14 +6,25 @@ function calcularValorHora(){
     const valorHora = document.getElementById("valor-hora").valueAsNumber
 
     const valorHoraTotal = (totalDias * horasDia) * valorHora
-document.getElementById("resultado-calculo").textContent = "Você receberá R$ " + valorHoraTotal
+document.getElementById("resultado-calculo").textContent = `Você receberá R$ ${valorHoraTotal.toFixed(2)}`
 }
 
 
-// Total do tempo =  dias x horas do dia
-// valor total horas = total do tempo x valor da hora
+
+function calcularValorFreela(){
+
+    const diasSemana = document.getElementById("dias-sem").valueAsNumber
+    const horasPorDia = document.getElementById("horas-por-dia").valueAsNumber
+    const ganhoMensal = document.getElementById("ganho-mensal").valueAsNumber
+
+    const horasPorSemana = diasSemana * horasPorDia
+    const totalHorasPorMes = horasPorSemana * 4 // quantidade de semanas no mês
+
+    const valorPorHoraMensal = ganhoMensal / totalHorasPorMes
+
+    console.log(valorPorHoraMensal);
+
+    document.getElementById("resultado-mensal").textContent = `você recebe R$ ${valorPorHoraMensal.toFixed(2)}`
 
 
-// valor total de horas = (dias x horas do dia) x valor da hora
-  
-                 //tempo     X   valor do tempo em horas
+}
